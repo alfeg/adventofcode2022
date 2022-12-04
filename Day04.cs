7a4d5c72638dee7460f6a57e62b906d6db1dec01
Viktor Hladkikh <viktor.hladkikh@continuity2.com>
2022-12-04 08:18:45 +0300

    day 4 complete

---
+++ Day04.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+
+namespace aoc;
+
+public class Day04 : AdventTest
+{
+    [TestCase("day4.sample", ExpectedResult = 2)]
+    [TestCase("day4.input", ExpectedResult = 644)]
+    public int Part1(string inputName)
+    {
+        return GetPairs(inputName)
+            .Count(p => p.A.FullOverlaps(p.B) || p.B.FullOverlaps(p.A));
+    }
+
+    [TestCase("day4.sample", ExpectedResult = 4)]
+    [TestCase("day4.input", ExpectedResult = 926)]
+    public int Part2(string inputName)
+    {
+        return GetPairs(inputName)
+            .Count(p => p.A.AnyOverlap(p.B) || p.B.AnyOverlap(p.A));
+    }
+
+    IEnumerable<(Range A, Range B)> GetPairs(string input)
+    {
+        return GetLines(input).Select(line =>
+        {
+            var split = line.Split(',');
+
+            Range Parse(string item)
+            {
+                var s = item.Split('-');
+                return new Range(int.Parse(s[0]), int.Parse(s[1]));
+            }
+
+            return (Parse(split[0]), Parse(split[1]));
+        });
+    }
+}
+
+record Range(int a, int b)
+{
+    public bool FullOverlaps(Range r)
+    {
+        return this.a <= r.a && this.b >= r.b;
+    }
+
+    public bool AnyOverlap(Range r)
+    {
+        // ----a------b---
+        //r-------a------b
+        
+        // ----a------b---
+        //r---a-----b------
+        return r.a <= a && r.b >= a || r.b <= b && r.b >= b;
+    }
+}
--- README.md
+++ README.md
@@ -9,13 +9,12 @@ https://adventofcode.com/2022
 dotnet test
 ```
 
-These are your personal leaderboard statistics. Rank is your position on that leaderboard: 1 means you were the first person to get that star, 2 means the second, 100 means the 100th, etc. Score is the number of points you got for that rank: 100 for 1st, 99 for 2nd, ..., 1 for 100th, and 0 otherwise.
-
 ```
       --------Part 1---------   --------Part 2---------
 Day       Time    Rank  Score       Time    Rank  Score
-  3   14:05:43   80845      0   14:17:25   74676      0
-  2       >24h  131173      0       >24h  125868      0
-  1   03:35:37   28867      0   03:37:21   26909      0
+4   00:08:01    3324      0   00:16:08    5217      0     - hmmm, ok =) That's a lot of people playing )
+3   14:05:43   80845      0   14:17:25   74676      0
+2       >24h  131173      0       >24h  125868      0
+1   03:35:37   28867      0   03:37:21   26909      0
 
 ```
